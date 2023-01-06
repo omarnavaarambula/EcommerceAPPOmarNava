8be28d056729b41903cbfa2c717a22a95a48a00a
omarnavaarambula <106714513+omarnavaarambula@users.noreply.github.com>
2023-01-05 20:37:52 -0600

    Update CartInfo.jsx

--- src/components/Cart/CartInfo.jsx
+++ src/components/Cart/CartInfo.jsx
@@ -7,7 +7,6 @@ import getConfig from '../../utils/getConfig'
 const CartInfo = ({productCart}) => {
   const dispatch = useDispatch()
   const deleteProductFromCart = () => {
-//     const URL = `https://ecommerce-api-react.herokuapp.com/api/v1/cart/${productCart.id}`
         const URL = `https://e-commerce-api.academlo.tech/api/v1/cart/${productCart.id}`
     axios.delete(URL, getConfig())
       .then(res => {
