ec3d7cc0525c3ea4464b403802cf3a13c98bc18e
omarnavaarambula <106714513+omarnavaarambula@users.noreply.github.com>
2023-01-05 20:43:06 -0600

    Update PurchasesScreen.jsx

--- src/components/Purchases/PurchasesScreen.jsx
+++ src/components/Purchases/PurchasesScreen.jsx
@@ -9,7 +9,6 @@ const PurchasesScreen = () => {
   const [purchases, setPurchases] = useState()
 
   useEffect(() => {
-//     const URL = 'https://ecommerce-api-react.herokuapp.com/api/v1/purchases'
     const URL = 'https://e-commerce-api.academlo.tech/api/v1/purchases'
     
     axios.get(URL, getConfig())
